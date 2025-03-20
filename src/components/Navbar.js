2d886cdb6388fe9149e1c989c24a73c7fe519558
ns-develops <test.tt9061030@gmail.com>
2025-03-20 20:31:08 +0100

    fixed icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,20 @@
-import React, {useState} from 'react'
-import { Link } from 'react-router-dom'
-
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitch } from '@fortawesome/free-brands-svg-icons'; // Importera faTwitch-ikonen
 
 function Navbar() {
   return (
-   <>
-   <nav className="navbar">
-    <div className="navbar-container">
-    <Link to="/" className="navbar-logo">
-     Your company logo <i className="fa-brands fa-twitch"></i>
-    </Link>
-    </div>
-   </nav>
-   </>
-
-  )
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            Your company logo <FontAwesomeIcon icon={faTwitch} /> {/* Använd ikonen här */}
+          </Link>
+        </div>
+      </nav>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
