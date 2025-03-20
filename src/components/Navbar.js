7e1f1d10a14ff09bc814cd6b49d0f6941532100b
ns-develops <test.tt9061030@gmail.com>
2025-03-20 21:46:32 +0100

    navbar ready

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,61 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTwitch } from '@fortawesome/free-brands-svg-icons'; // Importera faTwitch-ikonen
+import { faTwitch } from '@fortawesome/free-brands-svg-icons';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'; 
+import './Navbar.css';
+import { Button } from './Button';
 
 function Navbar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true)
+
+  const handleClick = () => setClick(!click); 
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if(window.innerWidth <= 960) {
+        setButton(false);
+    } else {
+        setButton(true);
+    }
+  };
+
+  window.addEventListener('resize', showButton);
+
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo">
-            Your company logo <FontAwesomeIcon icon={faTwitch} /> {/* Använd ikonen här */}
+            Your company logo <FontAwesomeIcon icon={faTwitch} />
           </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <FontAwesomeIcon icon={click ? faTimes : faBars} />
+          </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+          <li className='nav-item'>
+            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+            About
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link to='/cases' className='nav-links' onClick={closeMobileMenu}>
+            Cases
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link to='/solutions' className='nav-links' onClick={closeMobileMenu}>
+            Solutions
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link to='/contactus' className='nav-links' onClick={closeMobileMenu}>
+            Contact us
+            </Link>
+          </li>
+          </ul>
+          {button && <Button buttonStyle='btn--outline'>CONTACT US</Button>}
         </div>
       </nav>
     </>
