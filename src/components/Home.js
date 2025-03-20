63fcf62e347387cd897b81b964a7d1c52a20ca45
ns-develops <test.tt9061030@gmail.com>
2025-03-20 20:03:08 +0100

    navbar and home ground

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Välkommen till hemsidan!</h1>
+      <p>Här är startsidan.</p>
+    </div>
+  );
+}
+
+export default Home;
