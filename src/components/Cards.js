559cbbe8418a2dca2d1b28a1be6430addf7638f1
ns-develops <test.tt9061030@gmail.com>
2025-03-21 18:23:48 +0100

    text, before fixing picture issue

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,7 +1,6 @@
-import React from 'react'
+import React from 'react';
 import CardItem from './CardItem';
 import './Cards.css';
-
 
 function Cards() {
   return (
@@ -10,12 +9,19 @@ function Cards() {
         <div className="cards__container">
         <div className="cards__wrapper">
         <ul className="cards__items">
-        <CardItem/>
+        <CardItem
+        src="/design.jpg" 
+        text="Building an app is a comprehensive process that 
+        requires careful planning and collaboration. From concept workshop to launch, we ensure that your app is not 
+        only functional but also delivers an excellent user experience."
+        label="Building your app"
+        path="/solutions"
+        />
         </ul>
         </div>
         </div>
     </div>
-  )
+  );
 }
 
-export default Cards
+export default Cards;
