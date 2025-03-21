db434faf81acf07d1bb285a41f02d49fd03a7186
ns-develops <test.tt9061030@gmail.com>
2025-03-21 17:30:19 +0100

    adding process section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import '../App.css';
 import HeroSection from './HeroSection';
+import Cards from './Cards';
 
 
 function Home() {
   return (
     <div>
         <HeroSection/>
+        <Cards/>
       <h1>Välkommen till hemsidan!</h1>
       <p>Här är startsidan.</p>
     </div>
