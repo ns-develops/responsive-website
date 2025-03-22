bcce586b9e6044a74619e5498ac73336e680c51d
ns-develops <test.tt9061030@gmail.com>
2025-03-22 08:25:41 +0100

    fixing mute on video-demo

--- src/Demo.js
+++ src/Demo.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import './Demo.css'; // Din CSS-fil för layout
+import './Demo.css'; 
 
 const Demo = () => {
   return (
     <div className="demo">
-      {/* Använd en iframe för att bädda in YouTube-videon */}
       <iframe
         className="video-background"
         width="100%"
         height="100%"
-        src="https://www.youtube.com/embed/yKCAna4SrOA?autoplay=1&loop=1&mute=1&playlist=yKCAna4SrOA"
+        src="https://www.youtube.com/embed/yKCAna4SrOA?autoplay=1&loop=1&playlist=yKCAna4SrOA"
         title="Demo Video"
         frameBorder="0"
         allow="autoplay; encrypted-media"
@@ -18,5 +17,6 @@ const Demo = () => {
     </div>
   );
 }
+
 
 export default Demo;
